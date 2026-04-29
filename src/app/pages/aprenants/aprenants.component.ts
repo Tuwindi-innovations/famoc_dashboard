a05c58144a179d7b82d428d92f8c4bffa4a04fe5
Abdoul03 <samabdoul03@gmail.com>
2026-04-29 22:05:46 +0000

    ajout de la page d'apprenant

--- src/app/pages/aprenants/aprenants.component.ts
+++ src/app/pages/aprenants/aprenants.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { UserResponseDTO } from "src/app/models/User";
+import { AprenantsService } from "src/app/services/aprenants.service";
 
 @Component({
-  selector: 'app-aprenants',
-  templateUrl: './aprenants.component.html',
-  styleUrls: ['./aprenants.component.scss']
+  selector: "app-aprenants",
+  templateUrl: "./aprenants.component.html",
+  styleUrls: ["./aprenants.component.scss"],
 })
 export class AprenantsComponent implements OnInit {
+  private service = inject(AprenantsService);
+  private toastr = inject(ToastrService);
 
-  constructor() { }
+  apprenants: UserResponseDTO[] = [];
+  isLoading = true;
 
   ngOnInit(): void {
+    this.loadAll();
   }
 
+  loadAll() {
+    this.isLoading = true;
+    this.service.getAllApprenants().subscribe({
+      next: (data) => {
+        this.apprenants = data;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.toastr.error("Erreur de récupération");
+        this.isLoading = false;
+      },
+    });
+  }
+
+  onDelete(id: string) {
+    if (confirm("Confirmer la suppression de cet étudiant ?")) {
+      this.service.deleteApprenant(id).subscribe(() => {
+        this.apprenants = this.apprenants.filter((u) => u.id !== id);
+        this.toastr.success("Supprimé !");
+      });
+    }
+  }
+
+  onEdit(user: UserResponseDTO) {
+    // Logique pour ouvrir une modale de modification
+    console.log("Modifier :", user);
+  }
 }
--- src/app/services/aprenants.service.ts
+++ src/app/services/aprenants.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { UserResponseDTO } from "../models/User";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AprenantsService {
+  private serviceUrl: string;
+  private baseUrl: string = "apprenant";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.serviceUrl = environment.apiUrl;
+  }
+
+  getApprenant(id: string): Observable<UserResponseDTO> {
+    return this.http.get<UserResponseDTO>(
+      `${this.serviceUrl}/${this.baseUrl}/${id}`
+    );
+  }
+
+  getAllApprenants(): Observable<UserResponseDTO[]> {
+    return this.http.get<UserResponseDTO[]>(
+      `${this.serviceUrl}/${this.baseUrl}`
+    );
+  }
+
+  deleteApprenant(id: string): Observable<boolean> {
+    return this.http.delete<boolean>(
+      `${this.serviceUrl}/${this.baseUrl}/${id}`
+    );
+  }
 }
